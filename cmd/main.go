54cdf544372cc0813ad92cea7cc63b284eee13c1
Tajale72 <tajale72@gmail.com>
2023-04-30 18:43:49 -0500

    system info

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
@@ -12,7 +16,28 @@ func main() {
 	config := cors.DefaultConfig()
 	config.AllowAllOrigins = true
 	r.Use(cors.New(config))
+	r.GET("/systeminfo", SystemInfo)
 	r.POST("/upload", router.FileUpload)
 	r.Run()
 
+}
+
+type SystemConfig struct {
+	Host string   `json:"host" default:"localhost"`
+	Port int      `json:"port" default:"9000"`
+	Env  []string `json:"env"`
+}
+
+func SystemInfo(c *gin.Context) {
+	var system SystemConfig
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error getting hostname:", err)
+		return
+	}
+	env := os.Environ()
+	system.Env = env
+	system.Host = hostname
+
+	c.JSON(http.StatusAccepted, system)
 }
